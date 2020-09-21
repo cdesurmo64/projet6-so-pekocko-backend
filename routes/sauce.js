dcfbe2509dd0fbf46021f3fe33c4dd4db40925f5
Céline Desurmont <cdesurmo@gmail.com>
2020-09-21 15:40:31 +0200

    Update sauce router : Imported Sauce controller + modified its name in routes

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,22 +1,24 @@
 const express = require('express'); // Useful to create a router
 const router = express.Router(); // Creates router
 
+const sauceCtrl = require('../controllers/sauce') // Imports Sauce controller
+
 // POST route to create a sauce & save it in the DB
-router.post('/', SauceCtrl.createSauce);
+router.post('/', sauceCtrl.createSauce);
 
 // POST route to set the like status of a sauce (like, dislike or neutral)
-router.post('/:id/like', SauceCtrl.setLikeStatus);
+router.post('/:id/like', sauceCtrl.setLikeStatus);
 
 // Individual GET route to get info on one sauce from the DB
-router.get('/:id', SauceCtrl.getOneSauce);
+router.get('/:id', sauceCtrl.getOneSauce);
 
 // Individual PUT route to modify a sauce in the DB
-router.put('/:id', SauceCtrl.modifySauce);
+router.put('/:id', sauceCtrl.modifySauce);
 
 // Individual DELETE route to delete a sauce from the DB
-router.delete('/:id', SauceCtrl.deleteSauce);
+router.delete('/:id', sauceCtrl.deleteSauce);
 
 // Global GET route to get all sauces info from the DB
-router.get('/', SauceCtrl.getAllSauces);
+router.get('/', sauceCtrl.getAllSauces);
 
 module.exports = router; // Exports the Sauce router
