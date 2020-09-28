d8b630983b390429766cf46f2471db3cf517d070
Céline Desurmont <cdesurmo@gmail.com>
2020-09-28 11:08:38 +0200

    Authentication middleware : Created it & implemented the verification of the token sent by the frontend with the request (using jsonwebtoken package)

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken'); // Useful to decode a token
+
+// Authentication middleware
+module.exports = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]; // Gets the token from the request header 'Authorization'
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); // Decodes the token thanks to the secret key we used to create the token
+        const userId = decodedToken.userId; // Gets the userId contained in the decoded token
+        if (req.body.userId && req.body.userId !== userId) { // If there is a userId in the request body and if it's not the same as the one in the token...
+            throw 'User ID non valable'; // displays an error
+        } else { // If userIds match = our user is authenticated...
+            next(); // passes the execution to the next middleware
+        }
+    } catch(error) { // Catches the possible errors in try{} execution
+        res.status(401).json({ error: error.message | 'Requête non authentifiée' }); // If an error is collected -> displays its msg, otherwise displays the error msg written
+    }
+};
