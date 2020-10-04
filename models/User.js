bd28b1303415e74bd7c6d8cc8ebc0517337f98f6
Céline Desurmont <cdesurmo@gmail.com>
2020-10-04 14:15:53 +0200

    Updated User model : used the match property with a regex for the email attribute to make sure the email entered has the right syntax (when signing up)

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const uniqueValidator = require('mongoose-unique-validator'); // Useful for pre-
 
 // Creates the User data schema, including all the info the User objects will need
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true }, // To make sure 2 users can't have the same email address
-    password: { type: String, required: true }
+    email: { type: String, required: true, unique: true, match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Veuillez saisir une adresse email valide !']}, // To make sure 2 users can't have the same email address, and that the email entered has the right syntax
+    password: { type: String, required: true}
 });
 
 userSchema.plugin(uniqueValidator); // Defines uniqueValidator as a plugin for the User schema
